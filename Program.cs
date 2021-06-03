7b6708b677c02f0c90f4cca47865523f3d98bd58
AleWilson7 <ale.wilson7@gmail.com>
2021-06-03 14:13:20 -0300

    Commitando primeiro projeto .NET

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumeroDeVezes = 5;
+            for (int i=0; i <= NumeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
